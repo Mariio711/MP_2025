889249bfa028159a4a25a86e94734692231400cb
Mariio711 <80849476+Mariio711@users.noreply.github.com>
2025-07-23 13:37:31 +0200

    estructura recursiva hecha ej 8

--- Recursividad/Ej8.c
+++ Recursividad/Ej8.c
@@ -1,3 +1,31 @@
 /*Dado un vector A[1..n] de n enteros estrictamente positivos, siendo n ≥ 1, diseñe una función
 recursiva que obtenga el número de parejas (j, k) que cumplan:*/
 
+int llamada_parejas(int *A, int n);
+int parejas(int *A, int n, int j, int k, int cont);
+int sum_vec_rec(int *A, int n, int i, int sum);
+
+int llamada_parejas(int *A, int n)
+{
+    return parejas(A, n, 0, 0, 0);
+}
+
+int parejas(int *A, int n, int j, int k, int cont)
+{
+    if (j >= n)
+    {
+        return cont;
+    }
+
+    if(k >= n)
+    {
+        return parejas(A, n, j + 1, 0, cont);
+    }
+    
+    if (sum_vec_rec(A, j, 0, 0) == sum_vec_rec(A, n, k, 0))
+    {
+        cont++;
+    }
+
+    return parejas(A, n, j, k + 1, cont);
+}
